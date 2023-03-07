3c72d2bb3a4659e2c44d4bc7e2bc74fce6fc2bcf
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-03-07 15:29:17 +0200

    Update antarctica.js

--- World Map/westAntarctica/antarctica.js
+++ World Map/westAntarctica/antarctica.js
@@ -34,7 +34,7 @@ function fill() {
         const button2 = document.createElement('button');
         button2.textContent = 'Още информация'
         button2.addEventListener('click', ()=>{
-            location.href = 'https://bg.wikipedia.org/wiki/Западен_шелфов_ледник'
+            window.open('https://bg.wikipedia.org/wiki/Западен_шелфов_ледник','_blank')
         })
         body.appendChild(button2)
     }
