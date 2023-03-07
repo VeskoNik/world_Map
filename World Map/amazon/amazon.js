6a322430cce190ed165626f94a4937d7029b7bae
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-03-07 14:52:59 +0200

    Update amazon.js

--- World Map/amazon/amazon.js
+++ World Map/amazon/amazon.js
@@ -1,6 +1,9 @@
 function fillBody() {
-    debugger
+
     const body = document.querySelector('body');
+    body.innerHTML = `    <video autoplay muted loop id="myVideo">
+    <source src="video.mp4" type="video/mp4">
+    </video>    `;
     const h1 = document.createElement("h1")
     h1.textContent = "Амазонска екваториална гора";
     body.appendChild(h1)
