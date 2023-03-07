a87e8b0063e40961f58b5a4728b73f8cff58b7d4
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-03-07 15:22:12 +0200

    Update amazon.js

--- World Map/amazon/amazon.js
+++ World Map/amazon/amazon.js
@@ -39,7 +39,7 @@ function fillBody() {
         button2.textContent = 'Още информация';
         body.appendChild(button2)
         button2.addEventListener('click' , ()=>{
-            location.href = 'https://bg.wikipedia.org/wiki/%D0%90%D0%BC%D0%B0%D0%B7%D0%BE%D0%BD%D1%81%D0%BA%D0%B0_%D0%B5%D0%BA%D0%B2%D0%B0%D1%82%D0%BE%D1%80%D0%B8%D0%B0%D0%BB%D0%BD%D0%B0_%D0%B3%D0%BE%D1%80%D0%B0'
+            window.open('https://bg.wikipedia.org/wiki/%D0%90%D0%BC%D0%B0%D0%B7%D0%BE%D0%BD%D1%81%D0%BA%D0%B0_%D0%B5%D0%BA%D0%B2%D0%B0%D1%82%D0%BE%D1%80%D0%B8%D0%B0%D0%BB%D0%BD%D0%B0_%D0%B3%D0%BE%D1%80%D0%B0','_blank')
         })
     }
 }
