9affc9d63ac2af852128e72d34c5d93ffd1d543e
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-03-08 14:24:14 +0200

    Update taiga.js

--- World Map/taiga/taiga.js
+++ World Map/taiga/taiga.js
@@ -1,9 +1,5 @@
 function fill() {
     const body = document.querySelector('body');
-    body.innerHTML = `
-    <video autoplay muted loop id="myVideo">
-    <source src="Taiga.mp4" type="video/mp4">
-    </video>    `;
     const h1 = document.createElement("h1")
     h1.textContent = "Бореална Гора/Тайга";
     body.appendChild(h1)
