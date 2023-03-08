952457d7588652fabd69e3f636d75112493ca949
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-03-08 14:22:42 +0200

    Update mountainGletchers.js

--- World Map/mountainGletchers/mountainGletchers.js
+++ World Map/mountainGletchers/mountainGletchers.js
@@ -1,9 +1,5 @@
 function fill() {
     const body = document.querySelector('body');
-    body.innerHTML = `
-    <video autoplay muted loop id="myVideo">
-    <source src="mountains.mp4" type="video/mp4">
-    </video>    `;
     const h1 = document.createElement("h1")
     h1.textContent = "Планински Глетчери(Ледници)";
     body.appendChild(h1)
