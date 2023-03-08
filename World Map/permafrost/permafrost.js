a284998beefe1aee0cf10dfdd74dbccc3dcb0bfd
Veselin Nikolov <124354161+VeskoNik@users.noreply.github.com>
2023-03-08 14:23:18 +0200

    Update permafrost.js

--- World Map/permafrost/permafrost.js
+++ World Map/permafrost/permafrost.js
@@ -1,9 +1,5 @@
 function fill() {
     const body = document.querySelector('body');
-    body.innerHTML = `
-    <video autoplay muted loop id="myVideo">
-    <source src="permafrost.mp4" type="video/mp4">
-    </video>    `;
     const h1 = document.createElement("h1")
     h1.textContent = "Пермафрост";
     body.appendChild(h1)
